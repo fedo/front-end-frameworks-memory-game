511f80591743daff5af6f56b2d4a6a47808ea956
Federico Roffi <fedoverseas@gmail.com>
2015-08-15 03:19:11 +0100

    [om] Implemented the interface. Added images.

--- fefwmega-om/src/fefwmega_om/core.cljs
+++ fefwmega-om/src/fefwmega_om/core.cljs
@@ -6,18 +6,46 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text "Hello world!"
+                      :game (game/new-game)}))
+
+
 
 (om/root
-  (fn [data owner]
+  (fn [app-state owner]
     (reify om/IRender
       (render [_]
-        (println (game/new-game))
         (sab/html [:div.mdl-layout__container
                    [:div.mdl-layout.mdl-js-layout.mdl-layout--fixed-header
                     [:header.mdl-layout__header.is-casting-shadow
                      [:div.mdl-layout__header-row
-                      [:span.mdl-layout-title "Front-End Frameworks Memory Game"]]]]]))))
+                      [:span.mdl-layout-title "Front-End Frameworks Memory Game"]]]
+                    [:div.mdl-layout__content
+                     (into [:div.page-content]
+                       (vector (map-indexed (fn [line cards]
+                                              (into [:div.mdl-grid]
+                                                (vector (map-indexed
+                                                          (fn [column card]
+                                                            [:div.mdl-cell.mdl-typography--text-center
+                                                             {:on-click #(om/update! app-state [:game]
+                                                                          (let
+                                                                            [game (game/pick-card (-> app-state :game) line column)]
+                                                                            (println game)
+                                                                            game))}
+                                                             [:div (str (-> card :selected))]
+                                                             (let
+                                                               [src (str "images/"
+                                                                      (case (-> card :value)
+                                                                        0 "angular.png"
+                                                                        1 "gulp.png"))]
+                                                               [:img {:src (if (or (-> card :flipped)
+                                                                                 (-> card :picked))
+                                                                             src
+                                                                             "images/yeoman.png")}])])
+                                                          cards))
+                                                )
+                                              )
+                                 (-> app-state :game :cards))))]]]))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
