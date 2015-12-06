6381516e4fe187d0655032bd7a03eb5689b275d1
Federico Roffi <fedoverseas@gmail.com>
2015-12-06 03:01:21 +0000

    Made cards fully responsive.

--- fefwmega-om/src/fefwmega_om/core.cljs
+++ fefwmega-om/src/fefwmega_om/core.cljs
@@ -17,39 +17,54 @@
   (fn [app-state owner]
     (reify om/IRender
       (render [_]
-       (let [cards(-> app-state :game :cards)]
-         (sab/html [:div.mdl-layout__container
-                    [:div.mdl-layout.mdl-js-layout.mdl-layout--fixed-header
-                     [:header.mdl-layout__header.is-casting-shadow
-                      [:div.mdl-layout__header-row
-                       [:span.mdl-layout-title "Front-End Frameworks Memory Game"]]]
-                     [:div.mdl-layout__content
-                      (into [:div.page-content]
-                        (vector
-                          (map-indexed
-                            (fn [line-idx line]
-                              (into [:div.mdl-grid.mdl-grid--no-spacing]
-                                (vector (map-indexed
-                                          (fn [column-idx card]
-                                            [:div.mdl-typography--text-center
-                                             {:style
-                                                        {:height (str (/ 100 (count line)) "vw")
-                                                         :width (str (/ 100 (count line)) "vw")}
-                                              :on-click #(om/update! app-state [:game]
-                                                          (let
-                                                            [game (game/pick-card (-> app-state :game) line-idx column-idx)]
-                                                            (println game)
-                                                            game))}
-                                             [:div (str (-> card :selected))]
-                                             (let
-                                               [src (str "images/"
-                                                      (get images (:value card)))]
-                                               [:img {:src (if (or (-> card :flipped)
-                                                                   (-> card :picked))
-                                                             src
-                                                             "images/yeoman.png")}])])
-                                          line))))
-                            cards)))]]])) )))
+        (let [cards (-> app-state :game :cards)]
+          (sab/html [:div.mdl-layout__container
+                     [:div.mdl-layout.mdl-js-layout.mdl-layout--fixed-header
+                      [:header.mdl-layout__header.is-casting-shadow
+                       [:div.mdl-layout__header-row
+                        [:span.mdl-layout-title "Front-End Frameworks Memory Game"]]]
+                      [:div.mdl-layout__content
+                       (into [:div.page-content]
+                         (vector
+                           (map-indexed
+                             (fn [line-idx line]
+                               (into [:div.mdl-grid.mdl-grid--no-spacing
+                                      {:style {:display "table"
+                                               :margin "0 auto"}}]
+                                 (vector (map-indexed
+                                           (fn [column-idx card]
+                                             (let [top-padding "90px"
+                                                   calc-height (fn [n-columns unit]
+                                                                 (str (/ 100 n-columns) unit))
+                                                   adjust-height (fn [n-lines]
+                                                                   (str "calc(" (/ 100 n-lines) "vh - " (str (/ 64 n-lines)) "px)"))]
+                                               [:div.mdl-typography--text-center
+                                              {:style
+                                               {:display :inline-block
+                                                :max-height (adjust-height (count cards))
+                                                :max-width  (adjust-height (count cards))
+                                                :height     (calc-height (count cards) "vw")
+                                                :width      (calc-height (count cards) "vw")}
+                                               :on-click
+                                               #(om/update! app-state [:game]
+                                                 (let
+                                                   [game (game/pick-card (-> app-state :game) line-idx column-idx)]
+                                                   (println game)
+                                                   game))}
+                                              [:div (str (-> card :selected))]
+                                              (let
+                                                [src (str "images/"
+                                                       (get images (:value card)))]
+                                                [:img {:style
+                                                       {:height "100%"
+                                                        :max-width "100%"}
+                                                       :src
+                                                       (if (or (-> card :flipped)
+                                                               (-> card :picked))
+                                                         src
+                                                         "images/yeoman.png")}])]))
+                                           line))))
+                             cards)))]]])))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
